4b16a7beaab100d61a9ebcce75f972660fae7ca6
andrewdoanutz <andrewdoanutz@gmail.com>
2018-08-15 10:32:13 -0700

    brotherhood basically done, just need to touch up on css at the end and get more pics

--- frontend/src/brotherhood.js
+++ frontend/src/brotherhood.js
@@ -1,21 +1,31 @@
 import React from "react";
 
 import Brotherhood_collage from "./components/brotherhood_collage";
+import "./css/brotherhood.css"
 
 const Brotherhood = () => {
   return (
     <div>
-      <div>Brotherhood</div>
-      <div>Brotherhood</div>
-      <div>Brotherhood</div>
+      <section id="broHeader">
+      <div className="bro-text">
+        <a id="bro-cover-text">
+                  Brotherhood
+        </a>
+        </div>
+      </section>
       <div>
+       
+        <p id="p-bro-text">
         As members of Theta Tau, we strive to maintain a strong brotherhood
         through social activities and events, personal talks, or just simply
         hanging out and studying together.
+        </p>
+        
       </div>
       <div>
         <Brotherhood_collage />
       </div>
+      
     </div>
   );
 };
