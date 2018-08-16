d1b64d0c2b326ef4ff6e1940c0225e056aa17819
Ethan Su <ethansu@Ethans-MBP-3.fios-router.home>
2018-08-16 13:19:54 -0700

    fix another naming error

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use("/api/users", users);
 app.use("/api/profile", profile);
 
 if (process.env.NODE_ENV == "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("frontend/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
