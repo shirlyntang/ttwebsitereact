3549aa9ba45895a45dc287b70ad95ff1637ca8a0
andrewdoanutz <andrewdoanutz@gmail.com>
2018-10-01 20:18:24 -0700

    title on homepage

--- frontend/src/components/Home/about_us.js
+++ frontend/src/components/Home/about_us.js
@@ -7,15 +7,12 @@ export default class AboutUs extends React.Component {
       <div>
         <section id="about-us">
           <div className="container-fluid">
-            <div className="row">
-              <div className="col-md-12">
                 <div id="wwa">
-                  <h1 id="who-we-are">Who we are</h1>
+                  <h1 id="who-we-are">Theta Tau</h1>
                   <p id="who-we-are-description">
                     The Largest and Oldest Co-Ed Enginnering Fraternity
                   </p>
-                </div>
-              </div>
+                
             </div>
             <div className="row">
               <div className="col-md-12 purpose">
