8fc0572cbbf0dde262b6304fd64c68ed0ff7c0cf
Shirlyn Tang <shirlyntang@gmail.com>
2020-12-27 21:29:06 -0800

    removed some profile info for alumni

--- frontend/src/components/Members/profileInfo.js
+++ frontend/src/components/Members/profileInfo.js
@@ -6,6 +6,31 @@ class ProfileHeader extends Component {
   render() {
     const { profile } = this.props;
 
+    if (profile.activeOrAlumni == "Active")
+    {
+      return (
+        <div className="container">
+       
+          <img className="image"
+            id="profileImage"
+            src={profile.uploadedFileCloudinaryUrl}
+            alt="profileimage1"
+          />
+          
+          <div className="info">
+            <div className="category">Name: </div> <div >{profile.name}</div><div/>
+            <div className="category">Year/Major: </div> <div >{profile.yearMajor}</div><div/>
+            <div className="category">Pledge Class: </div> <div >{profile.pledgeClass}</div><div/>
+            <div className="category">Hometown: </div> <div >{profile.hometown}</div><div/>
+            <div className="category">Why I joined Theta Tau: </div> <div >{profile.whyIJoinTT}</div><div/>
+            <div className="category">What I enjoy about Theta Tau: </div> < div >{profile.whatIEnjoyAboutTT}</div><div/>
+            <div className="category">About me: </div> < div >{profile.bio}</div><div/>
+            <div className="category">LinkedIn: </div> <div ><a href={profile.linkedIn}>Click Here</a></div><div/>
+            <div className="category">Status: </div> <div >{profile.activeOrAlumni}</div>
+          </div>
+        </div>
+      );
+    }
     return (
       <div className="container">
      
@@ -16,13 +41,9 @@ class ProfileHeader extends Component {
         />
         
         <div className="info">
-          <div className="category">Handle: </div> <div >{profile.handle}</div><div/>
-          <div className="category">Year/Major: </div> <div >{profile.yearMajor}</div><div/>
+          <div className="category">Name: </div> <div >{profile.name}</div><div/>
+          <div className="category">Major: </div> <div >{profile.major}</div><div/>
           <div className="category">Pledge Class: </div> <div >{profile.pledgeClass}</div><div/>
-          <div className="category">Hometown: </div> <div >{profile.hometown}</div><div/>
-          <div className="category">Why I joined Theta Tau: </div> <div >{profile.whyIJoinTT}</div><div/>
-          <div className="category">What I enjoy about Theta Tau: </div> < div >{profile.whatIEnjoyAboutTT}</div><div/>
-          <div className="category">About me: </div> < div >{profile.bio}</div><div/>
           <div className="category">LinkedIn: </div> <div ><a href={profile.linkedIn}>Click Here</a></div><div/>
           <div className="category">Status: </div> <div >{profile.activeOrAlumni}</div>
         </div>
