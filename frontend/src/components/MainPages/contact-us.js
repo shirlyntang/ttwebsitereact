a8f4e836ddef5e2773408b87efba60e8639d634e
Erin <erinpwoo@yahoo.com>
2020-12-28 22:49:11 -0800

    basic header

--- frontend/src/components/MainPages/contact-us.js
+++ frontend/src/components/MainPages/contact-us.js
@@ -1,8 +1,14 @@
 import React from "react";
+import "../../css/contact_us.css";
 
 const ContactUs = () => {
     return (
         <div>
+            <section id="main">
+            <div className="contact-text">
+            <h2>Contact Us</h2>
+            </div>
+            </section>
         </div>
     );
 };
